dcf586c5e8b340798eb6894cd4283674ff5eceda
Saul Flores <saulpolo95@gmail.com>
2017-08-09 15:55:52 -0700

    added main content

--- comp/home/content.js
+++ comp/home/content.js
@@ -6,24 +6,40 @@ const MobileContent = () => (
       <Row>
         <Col xs={12} sm={12} md={6} lg={6} xl={6}>
           <div id='left'>
-            left
+            <h2>Somos una compañía certificada en el programa C-TPAT por mas de 10 años</h2>
           </div>
         </Col>
         <Col xs={12} sm={12} md={6} lg={6} xl={6}>
-        <div id='right'>
-          right
-        </div>        </Col>
+          <div id='right'>
+            <img src='http://www.transportesvector.com.mx/img/c-tpat.png' />
+          </div>
+        </Col>
       </Row>
     </div>
     <style jsx>{`
       .mobile-content {
         height: 500px;
+
       }
 
       #left {
         background-color: #375651;
         height: 445px;
         width: 100%;
+        text-align: center;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 20px;
+      }
+
+      #right {
+        height: 445px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
       }
 
     `}</style>
